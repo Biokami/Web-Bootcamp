f7d3457b5a635ef8f8c38d3355165dd5766b1428
Bryan Felfoldy <biokami@gmail.com>
2017-07-10 12:41:20 -0500

    js exercizes star

---
+++ Js Basic/script.js
@@ -0,0 +1,9 @@
+var userName = prompt("What is your name?");
+var lastName = prompt("what is your last name?");
+var userAge = prompt("What is your age?");
+
+alert("Nice to meet you " + userName + " " + lastName);
+alert("Your age is, " + userAge + " years old.");
+
+console.log("Also great to meet you, " + userName + " " + lastName);
+console.log("Your age is, " + userAge + " years old.");
